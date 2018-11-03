4d67ed33b2782a19e82364f14a86215311b3e5b3
Carlos <crgarcia@live.com.ar>
2018-11-03 09:37:16 +0100

    Works with cosmos db

--- ProcessingQueueFunction/__init__.py
+++ ProcessingQueueFunction/__init__.py
@@ -3,6 +3,10 @@ import logging
 import azure.functions as func
 
 
-def main(msg: func.QueueMessage) -> None:
-    logging.info('Python queue trigger function processed a queue item: %s',
-                 msg.get_body().decode('utf-8'))
+def main(msg: func.QueueMessage) -> func.Document:
+    logging.info('Python queue trigger function processed a queue item: %s', msg.get_body().decode('utf-8'))
+
+    processeditem = '{"id":"' + msg.get_body().decode('utf-8') + '"}'
+    doc = func.Document.from_json(processeditem)
+
+    return doc
