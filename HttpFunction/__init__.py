47311e1a55393de5666bf1a736b4cf540b3ec48b
Carlos <crgarcia@live.com.ar>
2018-11-16 15:57:28 +0100

    adding load to HttpTrigger

--- HttpFunction/__init__.py
+++ HttpFunction/__init__.py
@@ -1,24 +1,47 @@
 import logging
-
 import azure.functions as func
+import platform
+import sys
+import time
+import timeit
+from datetime import datetime
+import json
+from decimal import Decimal, getcontext
 
+class DocumentData(): pass
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def doMath():
+    getcontext().prec=100
+
+    for x in range(1000000):
+        result = sum(1/Decimal(16)**k * (Decimal(4)/(8*k+1) - Decimal(2)/(8*k+4) - Decimal(1)/(8*k+5) - Decimal(1)/(8*k+6)) for k in range(100))
+    return str(result)
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    try:
+        startTime = datetime.utcnow()
+        result = doMath()
+        stopTime = datetime.utcnow()
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+        args = sys.argv
+
+        data = DocumentData()
+        data.start = f'{startTime:%Y-%m-%d %H:%M:%S.%f%z}' 
+        data.stop = f'{stopTime:%Y-%m-%d %H:%M:%S.%f%z}'
+        data.durationSeconds = (stopTime-startTime).total_seconds()
+        data.host = args[2]
+        data.port = args[4]
+        data.worker = args[6]
+        data.request = args[8]
+        data.plataform = sys.platform
+        data.node = platform.node()
+        data.result = result
+        data.version = "1" # Identifyier to filter logs
+
+        jsonData = json.dumps(data.__dict__)
+        logging.info(jsonData)
+        return func.HttpResponse(jsonData)
+    except Exception as e:
+        return func.HttpResponse( f"<p>Error: {str(e)}</p>")
